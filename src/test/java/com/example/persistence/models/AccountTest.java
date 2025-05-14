cfd89333bc562c53425b75eb1d33af08cdc2aa07
Jack <jbpiatt2020@gmail.com>
2025-05-14 16:46:48 -0400

    STRUCTURE - removed connection test and organized test folder

--- src/test/java/com/example/persistence/models/AccountTest.java
+++ src/test/java/com/example/persistence/models/AccountTest.java
@@ -1,4 +1,4 @@
-package com.example.model;
+package com.example.persistence.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -6,8 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-
-import com.example.persistence.models.Account;
 
 public class AccountTest
 {
